dc56d9e70e2e93861077fc6d07500c3f44409be0
Mitch Matthews <165244888+mitchmatthews@users.noreply.github.com>
2024-05-16 01:47:22 +0000

    starting point

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:square)
+end
+get("/squareroot/new") do
+  erb(:squareroot)
+end
+get("/payment/new") do
+  erb(:payment)
+end
+get("/random/new") do
+  erb(:random)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
